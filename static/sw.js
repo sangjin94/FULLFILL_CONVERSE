b41205ac3d2f912dab2286ef99e9b56dfdbcfcb3
sangjin94 <sangjin94@users.noreply.github.com>
2026-03-02 15:31:32 +0900

    Add PWA support (manifest, service worker, icons)

---
+++ static/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'pda-return-v1';
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/static/css/style.css',
+                '/static/js/app.js',
+                '/static/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => {
+            return caches.match(e.request);
+        })
+    );
+});
